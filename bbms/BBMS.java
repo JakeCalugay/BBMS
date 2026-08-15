a039109579109cc0ddb48dae9e48ead0699c8f54
JakeCalugay <altorblxjake@gmail.com>
2026-08-15 14:09:41 +0800

    Add files via upload

---
+++ bbms/BBMS.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.bbms;
+
+/**
+ *
+ * @author CompLabC209-PC46
+ */
+public class BBMS {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
